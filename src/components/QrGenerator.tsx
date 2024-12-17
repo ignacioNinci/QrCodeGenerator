08ce6fad7734d80236b1e53c3f0e2df95159b155
Ignacio Ninci <ininci@sofredigital.com.ar>
2024-12-17 16:14:21 -0300

    add download functionality

--- src/components/QrGenerator.tsx
+++ src/components/QrGenerator.tsx
@@ -10,6 +10,38 @@ interface QrGeneratorProps {
 }
 
 export const QrGenerator: React.FC<QrGeneratorProps> = ({ onBackMain, qrUrl }): JSX.Element => {
+
+
+  const downloadQRCode = (): void => {
+    const svg = document.querySelector(".qrCode") as SVGSVGElement;
+  
+    if (!svg) {
+      console.error("No se encontró ningún elemento <svg> en el DOM.");
+      return;
+    }
+  
+    const serializer = new XMLSerializer();
+    const svgData = serializer.serializeToString(svg);
+  
+    const blob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+  
+    const downloadLink = document.createElement("a");
+    const url = URL.createObjectURL(blob);
+    downloadLink.href = url;
+    downloadLink.download = "qrcode.svg";
+  
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+
+    URL.revokeObjectURL(url);
+  };
+
+  
+
+  
+
+
   return (
     <>
       <div className="container">
@@ -29,11 +61,14 @@ export const QrGenerator: React.FC<QrGeneratorProps> = ({ onBackMain, qrUrl }): 
             </div>
           </div>
           <div className="buttons-container">
-            <button>Download</button>
-            <button>Share</button>
+            <div onClick={downloadQRCode}>
+              <i className="fa-solid fa-download"></i>
+            </div>
           </div>
         </div>
       </div>
     </>
   );
 };
+
+
